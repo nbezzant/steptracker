9ec599cd7912c27683483fa3feaa3a243d44f1d5
nbezzant-trellis <nate@trellisgrowthpartners.com>
2026-04-04 16:11:53 -0600

    fix auth handler rewrite

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/:path*',
+        destination: 'https://steptracker-prod.firebaseapp.com/__/auth/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
